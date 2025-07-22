73ea606e491b217a9d2477f9100c6441a6917f60
ivan-em-mobile <ivan.paulino@vocacao.org.br>
2025-07-22 20:44:04 -0300

    Update comparacao.py

--- comparacao.py
+++ comparacao.py
@@ -2,7 +2,7 @@
 ##Ivan, Gustavo, 
 # Tarso, Fabricio, 
 # Rafa-Gatao, 
-# Gabriel, Victor
+# Gabriel, Victor, Renan
 
 #Qual seria o print e 
 # input para pedir os nomes?
@@ -22,30 +22,32 @@ print("3 - Listar nomes")
 print("4 - Sair")
 
 #faça um loop para pedir a opção do usuário
-
+while True:
+    
+    opcao = input("\nDigite a opção desejada (1-4): ")
  
     if opcao == "1":
-        #novo_nome = input("Digite o nome a ser adicionado: ")
-        #nomes.append(novo_nome)
-        #print(f"{novo_nome} foi adicionado à lista.")
-        print(f" foi adicionado à lista.")
+        novo_nome = input("Digite o nome a ser adicionado: ")
+        nomes.append(novo_nome)
+        print(f"{novo_nome} foi adicionado à lista.")
+        #print(f" foi adicionado à lista.")
     
     elif opcao == "2":
-        #nome_remover = input("Digite o nome a ser removido: ")
-        #if nome_remover in nomes:
-        #    nomes.remove(nome_remover)
-        #    print(f"{nome_remover} foi removido da lista.")
-        #else:
-            #print(f"{nome_remover} não está na lista.")
-        print(f" não está na lista.")    
+        nome_remover = input("Digite o nome a ser removido: ")
+        if nome_remover in nomes:
+            nomes.remove(nome_remover)
+            print(f"{nome_remover} foi removido da lista.")
+        else:
+            print(f"{nome_remover} não está na lista.")
+        #print(f" não está na lista.")    
     
     elif opcao == "3":
         print("\nLista de Nomes:")
-        #for nome in nomes:
-        #    print(nome)
+        for nome in nomes:
+            print(nome)
     
     elif opcao == "4":
-        print("Saindo do programa...")
+        print("Você saiu do programa - EXIT.")
         break
     
     else:
